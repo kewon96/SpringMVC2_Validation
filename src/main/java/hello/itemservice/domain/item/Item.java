f81846b623012aa13ac2b23d6c472d0b99cf1674
Kewon <48837736+kewon96@users.noreply.github.com>
2022-09-10 16:45:37 +0900

    validation_v3  - global error

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -9,7 +10,12 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
+/**
+ * ScriptAssert의 경우 복잡한 검사에 대한 대응을 하기 힘들어서 실전에선 되려 기피하는 편이다.
+ * "이런게 있다"라는 정도만 알고 지나가자
+ */
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
